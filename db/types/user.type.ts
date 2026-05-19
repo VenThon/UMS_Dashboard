f5438bfb38fdaad0279e051c2d0ba06c4a93ab18
THON-VEN <thon-ven@mptc.gov.kh>
2026-05-19 18:44:53 +0700

    feat: user role

---
+++ db/types/user.type.ts
@@ -0,0 +1,11 @@
+
+export const USER_ROLE = {
+    IT_MANAGER: "it_manager",
+    PROJECT_MANAGER: "project_manager",
+    BUSINESS_ANALYSIS: "business_analysis",
+    SYSTEM_ANALYSIS: "system_analysis",
+    PRODUCT_OWNER: "product_owner",
+    UX_UI_DESIGNER: "ux_ui_designer",
+    GRAPHIC_DESIGNER: "graphic_designer"
+
+} as const;
