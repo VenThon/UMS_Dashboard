3e71218cf46468a17d0ed452c84c9ecace2b9966
THON-VEN <thon-ven@mptc.gov.kh>
2026-06-18 14:36:58 +0700

    fix: dashboard layout by each roles

--- components/layout/infrastructure-operation/(components)/nav-dashboard-infrastructure-operation.tsx
+++ components/layout/infrastructure-operation/(components)/nav-dashboard-infrastructure-operation.tsx
@@ -12,7 +12,13 @@ import {
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
 
-import { Database, LucideIcon, NotebookPen, Repeat } from "lucide-react";
+import {
+  AudioWaveform,
+  Database,
+  LucideIcon,
+  NotebookPen,
+  Repeat,
+} from "lucide-react";
 import { useLocale } from "next-intl";
 
 interface SideBarMenuBtnProps {
@@ -83,13 +89,30 @@ export default function NavBarDashboardInfrastructureAndOperation() {
 
             <SideBarMenuBtn
               icon={Repeat}
-              href={["/dashboard/infrastructure-operation/request"]}
-              title="Request"
+              href={[
+                "/dashboard/infrastructure-operation/general-request",
+                "/dashboard/infrastructure-operation/general-request/create",
+                "/dashboard/infrastructure-operation/general-request/[id]",
+              ]}
+              title="General Request"
+            />
+            <SideBarMenuBtn
+              icon={AudioWaveform}
+              href={[
+                "/dashboard/infrastructure-operation/leave-request",
+                "/dashboard/infrastructure-operation/leave-request/create",
+                "/dashboard/infrastructure-operation/leave-request/[id]",
+              ]}
+              title="Leave Requeat"
             />
             <SideBarMenuBtn
               icon={NotebookPen}
-              href={["/dashboard/infrastructure-operation/report"]}
-              title="Rport"
+              href={[
+                "/dashboard/infrastructure-operation/report",
+                "/dashboard/infrastructure-operation/report/create",
+                "/dashboard/infrastructure-operation/report/[id]",
+              ]}
+              title="Dialy Rport"
             />
           </SidebarMenuItem>
         </SidebarMenu>
